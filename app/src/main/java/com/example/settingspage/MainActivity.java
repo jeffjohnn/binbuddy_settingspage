b899def4f860b028b746ab5e7e8311c9e02d798b
Jeff John <john-j8@ulster.ac.uk>
2024-12-02 17:58:33 +0000

    Update Account Page

--- app/src/main/java/com/example/settingspage/MainActivity.java
+++ app/src/main/java/com/example/settingspage/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.settingspage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find Button by its ID
+        Button accountButton = findViewById(R.id.btn_Account);
+
+        // Set an onClick listener for the button
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the Account activity
+                Intent intent = new Intent(MainActivity.this, Account.class);
+                startActivity(intent);
+            }
+        });
     }
 }
